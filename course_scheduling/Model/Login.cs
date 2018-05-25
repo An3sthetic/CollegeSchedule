6e73da15fcaed85e448fcf6b8cc14f61c4a7fa56
Shadman sakib <Shadmanabir@outlook.com>
2018-05-25 23:15:56 +0600

    project uploaded

---
+++ course_scheduling/Model/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace course_scheduling.Model
+{
+    public class Login
+    {
+        private crschContext context;
+
+        public String User { get; set; }
+
+        public string Password { get; set; }
+
+        public string type { get; set; }
+
+        
+    }
+}
